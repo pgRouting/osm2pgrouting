33e1cca57f1578fc205f8ad5468a0d2578196cca
daniel <daniel@d21bcd6f-0036-404f-87c1-d983b28855fd>
2010-07-01 06:17:54 +0000

    Ticket #211: applied miising include patch

--- src/XMLParser.cpp
+++ src/XMLParser.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstdio> 
 #include "stdafx.h"
 #include "XMLParser.h"
 
