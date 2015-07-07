dbf6c4f82d457472c8e6e529f2a3e730edc8b6b6
Mohammed Zia <mohammed.zia33@gmail.com>
2015-07-07 15:53:28 +0300

    modified pgr_breakGraph() made code more readable and dropped garbage column

--- sql/broken_graph.sql
+++ sql/broken_graph.sql
@@ -5,19 +5,16 @@ CREATE OR REPLACE FUNCTION pgr_brokenGraph(
         RETURNS SETOF VOID AS
 $body$
 DECLARE
-        sql1     text;
-        rec1    record;
-        sql2     text;
-        rec2    record;
+        sql1 text;
+        rec1 record;
+        sql2 text;
+        rec2 record;
         rec_count record;
         rec_single record;
-        graph_id  integer;
-        target  integer;
-        gids int [];
-        --point integer;
-        
+        graph_id integer;
+        gids int [];        
 BEGIN   
-        EXECUTE 'SELECT count(*) as count FROM information_schema.columns WHERE table_name= ''' || quote_ident(tbl) || ''' and column_name= ''' || quote_ident(clmn) || '''' INTO rec_count;
+        EXECUTE 'SELECT COUNT(*) AS count FROM information_schema.columns WHERE table_name= ''' || quote_ident(tbl) || ''' AND column_name= ''' || quote_ident(clmn) || '''' INTO rec_count;
         If (rec_count.count > 0) THEN
                 RAISE NOTICE 'Column % in the table % already exists!', quote_ident(clmn), quote_ident(tbl);
         ELSE
@@ -26,24 +23,24 @@ BEGIN
                 graph_id := 1;
                 While true
                         Loop
-                                -------- Assisgn the very first -1 row graph_id
-                                EXECUTE 'select * from ' || quote_ident(tbl) || ' where ' || quote_ident(clmn) || ' = -1 limit 1' INTO rec_single;
-                                EXECUTE 'update ' || quote_ident(tbl) || ' set ' || quote_ident(clmn) || ' = ' || graph_id || ' where gid = ' || rec_single.gid || '';
+                                -------- Assign the very first -1 row graph_id
+                                EXECUTE 'SELECT * FROM ' || quote_ident(tbl) || ' WHERE ' || quote_ident(clmn) || ' = -1 LIMIT 1' INTO rec_single;
+                                EXECUTE 'UPDATE ' || quote_ident(tbl) || ' SET ' || quote_ident(clmn) || ' = ' || graph_id || ' WHERE gid = ' || rec_single.gid || '';
 
                                 --------- search other rows with that particular graph_id
                                 while true
                                         loop
-                                                execute 'select count(*) from ' || quote_ident(tbl) || ' where ' || quote_ident(clmn) || ' = ' || graph_id || ' AND garbage = 0' into rec_count;
+                                                execute 'SELECT COUNT(*) FROm ' || quote_ident(tbl) || ' WHERE ' || quote_ident(clmn) || ' = ' || graph_id || ' AND garbage = 0' into rec_count;
                                                 if (rec_count.count > 0) then
-                                                        sql1 := 'select * from ' || quote_ident(tbl) || ' where ' || quote_ident(clmn) || ' = ' || graph_id || ' AND garbage = 0';
+                                                        sql1 := 'SELECT * FROm ' || quote_ident(tbl) || ' WHERE ' || quote_ident(clmn) || ' = ' || graph_id || ' AND garbage = 0';
                                                         for rec1 in execute sql1
                                                                 loop
-                                                                        sql2 := 'select * from ' || quote_ident(tbl) || ' where source = '|| rec1.source ||' or target = '|| rec1.source ||' or source = '|| rec1.target ||' or target = '|| rec1.target ||'';
+                                                                        sql2 := 'SELECT * FROM ' || quote_ident(tbl) || ' WHERE source = '|| rec1.source ||' OR target = '|| rec1.source ||' OR source = '|| rec1.target ||' OR target = '|| rec1.target ||'';
                                                                         for rec2 in execute sql2
                                                                                 loop
-                                                                                        EXECUTE 'update ' || quote_ident(tbl) || ' set ' || quote_ident(clmn) || ' = ' || graph_id || ' where gid = ' || rec2.gid || '';
+                                                                                        EXECUTE 'UPDATE ' || quote_ident(tbl) || ' SET ' || quote_ident(clmn) || ' = ' || graph_id || ' WHERE gid = ' || rec2.gid || '';
                                                                                 end loop;
-                                                                        execute 'update ' || quote_ident(tbl) || ' set garbage = 1 where gid = ' || rec1.gid || '';
+                                                                        execute 'UPDATE ' || quote_ident(tbl) || ' SET garbage = 1 WHERE gid = ' || rec1.gid || '';
                                                                 end loop;
                                                 else
                                                         exit;
@@ -51,13 +48,14 @@ BEGIN
                                         end loop;
                                 
                                 ------ Following is to exit the while loop. 0 means no more -1 id.
-                                EXECUTE 'select count(*) as count from ' || quote_ident(tbl) || ' WHERE ' || quote_ident(clmn) || ' = -1' INTO rec_count;
+                                EXECUTE 'SELECT COUNT(*) AS count FROM ' || quote_ident(tbl) || ' WHERE ' || quote_ident(clmn) || ' = -1' INTO rec_count;
                                 If (rec_count.count = 0) THEN
                                         EXIT;
                                 ELSE
                                         graph_id := graph_id + 1;
                                 END IF;
                         end loop;
+                EXECUTE 'ALTER TABLE ' || quote_ident(tbl) || ' DROP COLUMN garbage';
         END IF;
         EXCEPTION
                 WHEN internal_error THEN
