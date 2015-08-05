cc4bc7fbdb650fe46bacb25180db56a4f3ec9385
sarthak <sarthak0415@gmail.com>
2015-08-05 22:45:10 +0530

    Configuration code linted using cpplint.py

--- src/Configuration.h
+++ src/Configuration.h
@@ -1,4 +1,4 @@
-/***************************************************************************
+/*******************************	********************************************
  *   Copyright (C) 2008 by Daniel Wendt                                    *
  *   gentoo.murray@gmail.com                                               *
  *                                                                         *
@@ -17,60 +17,54 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
- 
-#ifndef CONFIGURATION_H
-#define CONFIGURATION_H
 
-#include "Type.h"
-#include "Class.h"
+#ifndef SRC_CONFIGURATION_H_
+#define SRC_CONFIGURATION_H__
 
-namespace osm
-{
+#include <string>
+#include <map>
+#include "./Type.h"
+#include "./Class.h"
+
+namespace osm {
 
 template< class Map >
-inline void ez_mapdelete( Map& c )
-{
-	typename Map::iterator it( c.begin() );
-	typename Map::iterator last( c.end() );
-	while( it!=last )
-	{
-		delete (*it++).second;
-	}
+inline void ez_mapdelete(Map& c) {
+    typename Map::iterator it(c.begin());
+    typename Map::iterator last(c.end());
+    while (it != last) {
+        delete (*it++).second;
+    }
 }
 
 template< class Vector >
-inline void ez_vectordelete( Vector& c )
-{
-	typename Vector::iterator it( c.begin() );
-	typename Vector::iterator last( c.end() );
-	while( it!=last )
-	{
-		delete (*it++);
-	}
+inline void ez_vectordelete(Vector& c) {
+    typename Vector::iterator it(c.begin());
+    typename Vector::iterator last(c.end());
+    while (it != last) {
+        delete (*it++);
+    }
 }
-          
 
 /**
 A configuration document.
 */
-class Configuration
-{
-public:
-	//! Map, which saves the parsed types
-	std::map<std::string, Type*> m_Types;
-	//std::map<long long, Type*> m_Types;
-public:
-
-	//! Constructor
-	Configuration();
-	//! Destructor
-	virtual ~Configuration();
-	//! add node to the map
-	void AddType( Type* t );
-	Type* FindType( std::string typeName );
-	Class* FindClass( std::string typeName, std::string className );
+class Configuration {
+ public:
+    // ! Map, which saves the parsed types
+    std::map<std::string, Type*> m_Types;
+    // std::map<long long, Type*> m_Types;
+ public:
+    // ! Constructor
+    Configuration();
+    // ! Destructor
+    virtual ~Configuration();
+    // ! add node to the map
+    void AddType(Type* t);
+    Type* FindType(std::string typeName);
+    Class* FindClass(std::string typeName, std::string className);
 };
 
 
-} // end namespace osm
+}  // end namespace osm
 #endif
