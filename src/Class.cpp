7feccbc4e34de72e0e8757ecab5a1f6886e2c369
sarthak <sarthak0415@gmail.com>
2015-08-05 22:25:52 +0530

    CLASS code cleaned using cpplint.py

--- src/Class.cpp
+++ src/Class.cpp
@@ -16,25 +16,28 @@
 *   along with this program; if not, write to the                         *
 *   Free Software Foundation, Inc.,                                       *
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-***************************************************************************/
-                  
-#include "stdafx.h"
-#include "Class.h"
+***************************************************************************/                  
+#include <string>
+#include "./stdafx.h"
+#include "./Class.h"
 
-namespace osm
-{
 
-Class::Class(long long id, std::string name, double priority, int default_maxspeed)
+namespace osm {
+
+Class::Class(
+    long long id,
+    std::string name,
+    double priority,
+    int default_maxspeed
+    )
 :
   id(id),
   name(name),
   priority(priority),
-  default_maxspeed(default_maxspeed)
-{
+  default_maxspeed(default_maxspeed) {
 }
 
-Class::~Class()
-{
+Class::~Class() {
 }
 
-} // end namespace osm
+}  // end namespace osm
