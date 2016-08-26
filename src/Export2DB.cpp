7d2c326c3125f4c72326b40ba8d14b110fe81027
Aakash Sharma <aakash.y.sharma@gmail.com>
2016-08-26 18:33:36 +0200

    More debug

--- src/Export2DB.cpp
+++ src/Export2DB.cpp
@@ -358,6 +358,7 @@ void Export2DB::exportNodes(const std::map<long long, Node*> &nodes) const {
             row_data += TO_STR(node->timestamp);
             row_data += "\n";
             PQputline(mycon, row_data.c_str());
+            std::cout << row_data.c_str();
         }
         PQputline(mycon, "\\.\n");
         PQendcopy(mycon);
