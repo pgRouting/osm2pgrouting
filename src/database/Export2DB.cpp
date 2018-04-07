32c1ffc207ec19700325b8d0f4afeec53453c90d
cayetanobv <cayetano.benavent@geographica.gs>
2018-04-07 12:20:31 +0200

    fixed zero division error - included max_speed_backward too

--- src/database/Export2DB.cpp
+++ src/database/Export2DB.cpp
@@ -377,7 +377,7 @@ void Export2DB::fill_source_target(
             "           WHEN one_way = 1 THEN -ST_length(geography(ST_Transform(the_geom, 4326))) / (maxspeed_backward::float * 5.0 / 18.0)"
             "           ELSE ST_length(geography(ST_Transform(the_geom, 4326))) / (maxspeed_backward::float * 5.0 / 18.0)"
             "             END "
-            " WHERE length_m IS NULL AND maxspeed_forward != 0;");
+            " WHERE length_m IS NULL AND maxspeed_backward !=0 AND maxspeed_forward != 0;");
     Xaction.exec(sql3);
 }
 
